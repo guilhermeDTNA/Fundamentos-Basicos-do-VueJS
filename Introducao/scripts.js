f06355a09a2ce099b213ad2f354ea951028442c2
Guilherme <gleite98@gmail.com>
2021-10-04 15:51:40 -0300

    Vendo reduçao da sintaxe, props computed e watch e iniciando com estilos

---
+++ Introducao/scripts.js
@@ -0,0 +1,51 @@
+new Vue({
+    el: '#app', //referencia um elemento do DOM
+    data: { //dados a serem manipulados
+        titulo: 'Usando VueJS', //dados e funções não podem ter mesmo nome
+        linkGoogle: "https://google.com.br",
+        linkHtml: '<a href="https://guilhermerocha.tk" target="_black">Portfólio</a>',
+        contador: 0,
+        x: 0,
+        y: 0,
+
+        estilo1: false
+    },
+    computed:{
+        resultado(){
+            return this.contador > 10 ? 'Maior do que 10' : 'Menor ou igual a 10'
+        },
+
+        aplicarEstilo(){
+            return{
+                b1: this.estilo1,
+                b2: !this.estilo1
+            }
+        }
+    },
+    watch:{ //propriedade que verifica sempre que um dado é alterado
+        contador(novo, antigo){
+            setTimeout(() => {
+                this.contador = 0;
+            }, 5000);
+        }
+    },
+    methods: {
+        alterarTitulo(event){
+            this.titulo = event.target.value 
+        },
+        saudacao: function(){
+            //console.log(this)
+            return this.titulo
+        },
+        somar(){
+            this.contador++
+        },
+        atualizarMouse(e){
+            this.x = e.clientX;
+            this.y = e.clientY;
+        },
+        notificaEnter(e){
+            alert("Apertou alt e enter");
+        }
+    }
+})
