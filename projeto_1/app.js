844aed4a51d1ae541cd7671c3c6686e92fff7f88
Guilherme <gleite98@gmail.com>
2021-11-15 19:18:22 -0300

    Implementando ataque e cura

--- projeto_1/app.js
+++ projeto_1/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
     data:{
         playerLife: 0,
-        monsterLife: 20,
+        monsterLife: 0,
         running: false
     },
     computed:{
@@ -15,9 +15,34 @@ new Vue({
             this.running = true;
             this.monsterLife = 100;
             this.playerLife = 100;
+        },
+        attack(special){
+            this.hurt('playerLife', 7, 12, false);
+            this.hurt('monsterLife', 5, 10, special);
+        },
+        hurt(atr, min, max, special){
+            const plus = special ? 5 : 0;
+            const hurt = this.getRandom(min + plus, max + plus);
+            this[atr] = Math.max(this[atr] - hurt, 0); //Se nº < 0, n = 0 (para não ficar negativo) 
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max-min) + min;
+            return Math.round(value);
+        },
+        healAndHurt(){
+            this.heal(10, 15);
+            this.hurt('playerLife', 7, 12, false);
+        },
+        heal(min, max){
+            const heal = this.getRandom(min, max);
+            this.playerLife = Math.min(this.playerLife + heal, 100); //Pega no máximo o valor 100
         }
     },
     watch:{
-
+        hasResult(value){
+            if(value){
+                this.running = false;
+            }
+        }
     }
 });
